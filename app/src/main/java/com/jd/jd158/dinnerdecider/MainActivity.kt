4588990c10e90718391c2286a25919a7b6fe2a0a
Jackson Dias da Silva <jdhc3m@gmail.com>
2018-02-10 16:51:21 +0800

    3 commit

--- app/src/main/java/com/jd/jd158/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/jd/jd158/dinnerdecider/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val foodList = arrayListOf("Pizza", "Nazigoren", "Baksddu", "Macarrao")
+        val foodList = arrayListOf("Pizza", "Nazigoren", "Baksddu", "Macarrao", "ovo")
         var nullable : String? = "Nullable test"
 
         var l = if (nullable != null) nullable.length else -1
