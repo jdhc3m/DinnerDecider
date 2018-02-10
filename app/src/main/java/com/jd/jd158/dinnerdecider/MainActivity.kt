1d7e65c7fb5d7b7093e465ab3b29ae9013d1a696
Jackson Dias da Silva <jdhc3m@gmail.com>
2018-02-10 16:26:46 +0800

    My first commit

---
+++ app/src/main/java/com/jd/jd158/dinnerdecider/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.jd.jd158.dinnerdecider
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val foodList = arrayListOf("Pizza", "Nazigoren", "Baksddu", "Macarrao")
+        var nullable : String? = "Nullable test"
+
+        var l = if (nullable != null) nullable.length else -1
+
+
+        decideBtn.setOnClickListener(){
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count());
+            foodNametxt.text = foodList[randomFood] 
+
+        }
+
+        addFoodBtn.setOnClickListener(){
+            foodList.add(addFoodText.text.toString())
+            addFoodText.text.clear()
+        }
+    }
+}
